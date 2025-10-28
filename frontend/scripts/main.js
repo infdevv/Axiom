0c5c92df0a0a691ef0b568a5fffc7186e371d336
infdevv <infdevv@proton.me>
2025-10-27 21:31:59 -0500

    v3

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -223,3 +223,28 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+
+document.onclick = hideMenu;
+        document.oncontextmenu = rightClick;
+
+        function hideMenu() {
+            document.getElementById(
+                "contextMenu").style.display = "none"
+        }
+
+        function rightClick(e) {
+            e.preventDefault();
+
+            if (document.getElementById(
+                "contextMenu").style.display == "block")
+                hideMenu();
+            else {
+                let menu = document
+                    .getElementById("contextMenu")
+
+                menu.style.display = 'block';
+                menu.style.left = e.pageX + "px";
+                menu.style.top = e.pageY + "px";
+            }
+        }
